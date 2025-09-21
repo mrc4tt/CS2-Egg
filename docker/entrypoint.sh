5b9fe48fbbd6442fad9f9b0db4dd3ba6c8ce2ed0
mrc4tt <mikkel.linddk@gmail.com>
2025-09-21 13:29:12 +0200

    hmm

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -92,13 +92,8 @@ if [ "${UPDATE_AUTO_RESTART:-0}" -eq 1 ]; then
     version_check_loop &
 fi
 
-# Prepare startup command
-# Convert {{VAR}} → ${VAR} but DO NOT eval here (keeps $(...) intact)
-REPLACED="$(printf '%s' "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')"
-
-# Run the command under bash -lc so ${VAR} expands at runtime
-# and any $(...) stays intact until the final exec
-MODIFIED_STARTUP="unbuffer -p bash -lc \"$REPLACED\""
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+MODIFIED_STARTUP="unbuffer -p ${MODIFIED_STARTUP}"
 
 # Log censored startup command
 LOGGED_STARTUP=$(echo "${MODIFIED_STARTUP#unbuffer -p }" | \
